66ddedc5d86a013f8b11b6da43535f970285fe07
parksangryong <ppsr3478@naver.com>
2025-05-09 15:43:17 +0900

    img src 수정

--- src/data/poket.ts
+++ src/data/poket.ts
@@ -1,27 +1,27 @@
 export const FIRE = [
   {
     id: 1,
-    image: "/src/assets/image/fire 1.png",
+    image: "/image/fire 1.png",
     word: "C",
   },
   {
     id: 2,
-    image: "/src/assets/image/fire 2.png",
+    image: "/image/fire 2.png",
     word: "H",
   },
   {
     id: 3,
-    image: "/src/assets/image/fire 3.png",
+    image: "/image/fire 3.png",
     word: "O",
   },
   {
     id: 4,
-    image: "/src/assets/image/fire 4.png",
+    image: "/image/fire 4.png",
     word: "R",
   },
   {
     id: 5,
-    image: "/src/assets/image/fire 5.png",
+    image: "/image/fire 5.png",
     word: "D",
   },
 ];
@@ -29,27 +29,27 @@ export const FIRE = [
 export const WATER = [
   {
     id: 1,
-    image: "/src/assets/image/water 1.png",
+    image: "/image/water 1.png",
     word: "O",
   },
   {
     id: 2,
-    image: "/src/assets/image/water 2.png",
+    image: "/image/water 2.png",
     word: "C",
   },
   {
     id: 3,
-    image: "/src/assets/image/water 3.png",
+    image: "/image/water 3.png",
     word: "H",
   },
   {
     id: 4,
-    image: "/src/assets/image/water 4.png",
+    image: "/image/water 4.png",
     word: "R",
   },
   {
     id: 5,
-    image: "/src/assets/image/water 5.png",
+    image: "/image/water 5.png",
     word: "D",
   },
 ];
@@ -57,27 +57,27 @@ export const WATER = [
 export const EARTH = [
   {
     id: 1,
-    image: "/src/assets/image/earth 1.png",
+    image: "/image/earth 1.png",
     word: "C",
   },
   {
     id: 2,
-    image: "/src/assets/image/earth 2.png",
+    image: "/image/earth 2.png",
     word: "R",
   },
   {
     id: 3,
-    image: "/src/assets/image/earth 3.png",
+    image: "/image/earth 3.png",
     word: "D",
   },
   {
     id: 4,
-    image: "/src/assets/image/earth 4.png",
+    image: "/image/earth 4.png",
     word: "O",
   },
   {
     id: 5,
-    image: "/src/assets/image/earth 5.png",
+    image: "/image/earth 5.png",
     word: "H",
   },
 ];
@@ -85,27 +85,27 @@ export const EARTH = [
 export const THUNDER = [
   {
     id: 1,
-    image: "/src/assets/image/thunder 1.png",
+    image: "/image/thunder 1.png",
     word: "D",
   },
   {
     id: 2,
-    image: "/src/assets/image/thunder 2.png",
+    image: "/image/thunder 2.png",
     word: "O",
   },
   {
     id: 3,
-    image: "/src/assets/image/thunder 3.png",
+    image: "/image/thunder 3.png",
     word: "H",
   },
   {
     id: 4,
-    image: "/src/assets/image/thunder 4.png",
+    image: "/image/thunder 4.png",
     word: "R",
   },
   {
     id: 5,
-    image: "/src/assets/image/thunder 5.png",
+    image: "/image/thunder 5.png",
     word: "C",
   },
 ];
